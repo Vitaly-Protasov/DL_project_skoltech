61843fdd15c3c5f0624373c3e12ee8451d71be07
Vitaly-Protasov <you@example.com>
2020-06-01 01:31:34 +0300

    fixed create_vocab

--- create_vocab.py
+++ create_vocab.py
@@ -8,22 +8,22 @@ def create_vocab(path):
         target2count = pickle.load(file)
         n_training_examples = pickle.load(file)
     
-    word2idx = {'<unk>': 0, '<pad>': 1}
-    path2idx = {'<unk>': 0, '<pad>': 1 }
-    target2idx = {'<unk>': 0, '<pad>': 1}
+        word2idx = {'<unk>': 0, '<pad>': 1}
+        path2idx = {'<unk>': 0, '<pad>': 1 }
+        target2idx = {'<unk>': 0, '<pad>': 1}
 
-    idx2target = {}
+        idx2target = {}
 
-    for w in word2count.keys():
-        word2idx[w] = len(word2idx)
-        
-    for p in path2count.keys():
-        path2idx[p] = len(path2idx)
-        
-    for t in target2count.keys():
-        target2idx[t] = len(target2idx)
-        
-    for k, v in target2idx.items():
-        idx2target[v] = k
-        
-    return word2idx, path2idx, target2idx, idx2target
+        for w in word2count.keys():
+            word2idx[w] = len(word2idx)
+            
+        for p in path2count.keys():
+            path2idx[p] = len(path2idx)
+            
+        for t in target2count.keys():
+            target2idx[t] = len(target2idx)
+            
+        for k, v in target2idx.items():
+            idx2target[v] = k
+            
+        return word2idx, path2idx, target2idx, idx2target
