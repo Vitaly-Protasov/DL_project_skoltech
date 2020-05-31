19503e2b2362460ab151db1a110057ef16b58221
Vitaly-Protasov <you@example.com>
2020-06-01 01:09:37 +0300

    fixed dataloader and model

--- data_to_tensors.py
+++ data_to_tensors.py
@@ -1,24 +1,30 @@
 import torch
 from tqdm.notebook import tqdm
 
-MAX_NUM_PATHS  = 200
 
-class DatasetBuiled:
+MAX_NUM_PATHS  = 200
+class DatasetBuilder:
     '''
     The purpose of this class is to process a dataset's 
     each line and form 3 tensors of start_words, contexts,
     end_words. Lines are looks like 'method_name start_word, 
     context, end_word'
     '''
-    def __init__(self, dataset_path, value_vocab, path_vocab):
+    def __init__(self, dataset_path, value_vocab, path_vocab, target_vocab, batch_size = 100):
         self.dataset_path = dataset_path
         self.value_vocab = value_vocab
         self.path_vocab = path_vocab
+        self.target_vocab = target_vocab
+        self.batch_size = batch_size
 
     def _data_processing_one_line(self, line):
+    '''
+    Processing for the each line of the dataset
+    '''
         final_start = []
         final_path = []
         final_ends = []
+        final_label = None
         name, *tree = line.split(' ')
         for each_path in tree:
             if each_path != '' and each_path != '\n':
@@ -31,25 +37,47 @@ class DatasetBuiled:
                   final_start.append(self.value_vocab[temp_path[0]])
                   final_path.append(self.path_vocab[temp_path[1]])
                   final_ends.append(self.value_vocab[temp_path[2]])
-
+                  
+        # in order to fulfil to the max number of paths
         final_start += [self.value_vocab['<pad>']] * (MAX_NUM_PATHS - len(final_start))
-        final_path += [self.value_vocab['<pad>']] * (MAX_NUM_PATHS - len(final_path))
+        final_path += [self.path_vocab['<pad>']] * (MAX_NUM_PATHS - len(final_path))
         final_ends += [self.value_vocab['<pad>']] * (MAX_NUM_PATHS - len(final_ends))
-        return final_start, final_path, final_ends
+        final_labels = self.target_vocab[name]
+        return final_start, final_path, final_ends, final_labels
 
     def _form_tensors(self):
-        list_starts = []
-        list_contexts = []
-        list_ends = []
+    '''
+    This method forms tensors which will be delivered to the model.
+    Also, it should be considered as the iterator for the dataset.
+    output: tensor_starts, tensor_contexts, tensor_ends, tensor_labels
+    '''
         with open(self.dataset_path, 'r') as file:
 
-            for line in tqdm(file):
-                        test_ = self._data_processing_one_line(line)
-                        list_starts += [test_[0]]
-                        list_contexts += [test_[1]]
-                        list_ends += [test_[2]]
+            list_starts = []
+            list_contexts = []
+            list_ends = []
+            list_labels = []
+            temp_tensor_labels = np.zeros(len(self.target_vocab))
+            
+            for i, line in enumerate(tqdm(file)):
+                test_ = self._data_processing_one_line(line)
+                list_starts += [test_[0]]
+                list_contexts += [test_[1]]
+                list_ends += [test_[2]]
 
-        tensor_starts = torch.LongTensor(list_starts)
-        tensor_contexts = torch.LongTensor(list_contexts)
-        tensor_ends = torch.LongTensor(list_ends)
-        return tensor_starts, tensor_contexts, tensor_ends
+                temp_tensor_labels[test_[3]] = 1
+                list_labels += [temp_tensor_labels.copy()]
+                temp_tensor_labels[test_[3]] = 0
+                
+                if (i+1) % self.batch_size == 0:
+                    tensor_starts = torch.LongTensor(list_starts)
+                    tensor_contexts = torch.LongTensor(list_contexts)
+                    tensor_ends = torch.LongTensor(list_ends)
+                    tensor_labels = torch.LongTensor(list_labels)
+
+                    list_starts = []
+                    list_contexts = []
+                    list_ends = []
+                    list_labels = []
+
+                    yield tensor_starts, tensor_contexts, tensor_ends, tensor_labels
--- model.py
+++ model.py
@@ -1,7 +1,3 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
 class code2vec_model(nn.Module):
   """
   self.values_vocab_size - set of values of AST terminals that were observed during training
@@ -11,14 +7,13 @@ class code2vec_model(nn.Module):
 
   """
   def __init__(self, 
-               values_vocab_size,
-               paths_vocab_size,
                val_embedding_dim = 10,
                path_embedding_dim = 10,
                dropout_rate = 0.25,
                embedding_dim = 10,
-               labels_num,
-               ):
+               values_vocab_size = 0,
+               paths_vocab_size = 0,
+               labels_num = 0):
     super().__init__()
 
     self.values_vocab_size = values_vocab_size
@@ -43,6 +38,8 @@ class code2vec_model(nn.Module):
     ## 4. Prediction
     self.output_linear = nn.Linear(self.embedding_dim, self.labels_num, bias = False)
 
+    self.neg_INF = - 2 * 10**10
+
   def forward(self, starts, paths, ends):
 
     """
@@ -64,8 +61,10 @@ class code2vec_model(nn.Module):
     comb_context_vec = torch.tanh(self.linear(context_vec)) 
 
     ## 4. Attention mechanism
-    attn_mask = (starts > 1).float()
-    attention_weights = F.softmax(torch.matmul(comb_context_vec, self.a.T), dim = 1)
+    mask = (starts > 1).float() ## if 1 then it is pad and we don't pay attention to it
+
+    lin_mul = torch.matmul(comb_context_vec, self.a.T)
+    attention_weights = F.softmax(torch.mul(lin_mul, mask.view(lin_mul.size())) + (1 - mask.view(lin_mul.size())) * self.neg_INF, dim = 1)
     code_vector = torch.sum(torch.mul(comb_context_vec, attention_weights), dim = 1)
 
     ## 5. Prediction
