8d8254a303ac0ae6e8ff238a8ec308d687f26ce8
Leggerla <52498629+Leggerla@users.noreply.github.com>
2020-06-03 02:30:10 +0300

    Update data_to_tensors.py

--- data_to_tensors.py
+++ data_to_tensors.py
@@ -1,8 +1,51 @@
 import torch
 import numpy as np
+import os
 
 
 MAX_NUM_PATHS  = 200
+
+class TextDataset(Dataset):
+    
+    def __init__(self, dataset_path, value_vocab, path_vocab, target_vocab, batch_size = 100):
+        self.dataset_path = dataset_path
+        self.value_vocab = value_vocab
+        self.path_vocab = path_vocab
+        self.target_vocab = target_vocab
+        self.batch_size = batch_size
+        self.len_dataset = sum(1 for line in open(dataset_path, 'r'))
+    
+    def __len__(self):
+        return self.len_dataset
+    
+    def __getitem__(self, idx):
+        line = linecache.getline(self.dataset_path, idx + 1)
+        
+    def _data_processing_one_line(self, line):
+        '''
+        Processing for the each line of the dataset
+        '''
+        final_start = []
+        final_path = []
+        final_ends = []
+        final_labels = None
+        name, *tree = line.split(' ')
+        for each_path in tree:
+            if each_path != '' and each_path != '\n':
+                temp_path = each_path.split(',')
+
+                final_start.append(self.value_vocab.get(temp_path[0], self.value_vocab['<unk>']))
+                final_path.append(self.path_vocab.get(temp_path[1], self.path_vocab['<unk>']))
+                final_ends.append(self.value_vocab.get(temp_path[2], self.value_vocab['<unk>']))
+                  
+        # in order to fulfil to the max number of paths
+        final_start += [self.value_vocab['<pad>']] * (MAX_NUM_PATHS - len(final_start))
+        final_path += [self.path_vocab['<pad>']] * (MAX_NUM_PATHS - len(final_path))
+        final_ends += [self.value_vocab['<pad>']] * (MAX_NUM_PATHS - len(final_ends))
+        final_labels = self.target_vocab.get(name, self.target_vocab['<unk>'])
+        return final_start, final_path, final_ends, final_labels
+
+        
 
 
 class DatasetBuilder:
