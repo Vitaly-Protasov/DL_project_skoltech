14626cb9007ed800e3f7009e733f91c544bed828
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-06-07 10:51:15 +0300

    Update model_implementation.py

--- model_implementation.py
+++ model_implementation.py
@@ -45,12 +45,11 @@ class code2vec_model(nn.Module):
       num_attention_heads = bert_params['num_attention_heads']
       num_transformer_layers = bert_params['num_transformer_layers']
       intermediate_size = bert_params['intermediate_size']
-      hidden_size = self.path_embedding_dim + 2 * self.val_embedding_dim
       self.scale = torch.nn.Parameter(torch.sqrt(torch.tensor(hidden_size, dtype=torch.float32)), requires_grad=False)
       max_len = 512
-      self.pos_embedding = nn.Embedding(max_len, hidden_size)
+      self.pos_embedding = nn.Embedding(max_len, self.val_embedding_dim)
       configuration = BertConfig(type_vocab_size=1, vocab_size=self.labels_num, 
-                                 hidden_size=hidden_size, num_attention_heads=num_attention_heads, 
+                                 hidden_size=self.val_embedding_dim, num_attention_heads=num_attention_heads, 
                                  num_hidden_layers=num_transformer_layers, intermediate_size=intermediate_size, 
                                  hidden_dropout_prob=dropout_rate, attention_probs_dropout_prob=dropout_rate, max_position_embeddings=max_len)
                                  
@@ -80,10 +79,13 @@ class code2vec_model(nn.Module):
     
     ## 3. Attention mechanism
     mask = (starts > 1).float() ## if 1 then it is pad and we don't pay attention to it
+    ## 4. DropOut + Fully-connected layer into 'Combinied context vectors'
+    context_vec = self.DropOut(context_vec)
+    comb_context_vec = torch.tanh(self.linear(context_vec))
     
     if self.bert:
-      bs, seq_len = context_vec.shape[:2]
-      token_embeds = self.scale * (context_vec*mask)
+      bs, seq_len = comb_context_vec.shape[:2]
+      token_embeds = self.scale * comb_context_vec
       positions = torch.arange(seq_len).to(device).repeat(bs).view(bs, seq_len)
       pos_embeds = self.pos_embedding(positions)
       sum_embeds = torch.add(token_embeds, pos_embeds)
@@ -91,9 +93,7 @@ class code2vec_model(nn.Module):
       _, code_vector = self.bert(attention_mask=mask, inputs_embeds=context_vec)
       output = self.output_linear(code_vector)
     else:
-      ## 4. DropOut + Fully-connected layer into 'Combinied context vectors'
-      context_vec = self.DropOut(context_vec)
-      comb_context_vec = torch.tanh(self.linear(context_vec)) 
+
       lin_mul = torch.matmul(comb_context_vec, self.a.T)
       attention_weights = F.softmax(torch.mul(lin_mul, mask.view(lin_mul.size())) + (1 - mask.view(lin_mul.size())) * self.neg_INF, dim = 1)
       code_vector = torch.sum(torch.mul(comb_context_vec, attention_weights), dim = 1)
